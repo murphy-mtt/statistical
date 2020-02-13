a3d7435b3f61a2a27c44a2519aa88ea90642c089
murphy-mtt <zd_wangfenglin@163.com>
2020-02-13 13:57:58 +0800

    sale percentile bar

--- my_plot.py
+++ my_plot.py
@@ -5,6 +5,8 @@ import matplotlib.patches as mpatches  # needed for waffle Charts
 import mpl_toolkits.axisartist as axisartist
 import matplotlib.dates as mdates
 from datetime import datetime
+from matplotlib.ticker import MaxNLocator
+from collections import namedtuple
 
 
 def my_plotter(ax, data1, data2, para):
@@ -303,9 +305,175 @@ def timeline(names, dates):
     plt.savefig("/home/murphy/django/static/images/stat.png")
 
 
+def attach_ordinal(num):
+    """helper function to add ordinal string to integers
+
+    1 -> 1st
+    56 -> 56th
+    """
+    suffixes = {str(i): v
+                for i, v in enumerate(['th', 'st', 'nd', 'rd', 'th',
+                                       'th', 'th', 'th', 'th', 'th'])}
+
+    v = str(num)
+    # special case early teens
+    if v in {'11', '12', '13'}:
+        return v + 'th'
+    return v + suffixes[v[-1]]
+
+
+def format_score(scr, test):
+    """
+    Build up the score labels for the right Y-axis by first
+    appending a carriage return to each string and then tacking on
+    the appropriate meta information (i.e., 'laps' vs 'seconds'). We
+    want the labels centered on the ticks, so if there is no meta
+    info (like for pushups) then don't add the carriage return to
+    the string
+    """
+    md = testMeta[test]
+    if md:
+        return '{0}\n{1}'.format(scr, md)
+    else:
+        return scr
+
+
+def format_ycursor(y):
+    y = int(y)
+    if y < 0 or y >= len(testNames):
+        return ''
+    else:
+        return testNames[y]
+
+
+def plot_student_results(student, scores, cohort_size):
+    #  create the figure
+    fig, ax1 = plt.subplots(figsize=(9, 7))
+    fig.subplots_adjust(left=0.115, right=0.88)
+    fig.canvas.set_window_title('Eldorado K-8 Fitness Chart')
+
+    pos = np.arange(len(testNames))
+
+    rects = ax1.barh(pos, [scores[k].percentile for k in testNames],
+                     align='center',
+                     height=0.5,
+                     tick_label=testNames)
+
+    ax1.set_title(student.name)
+
+    ax1.set_xlim([0, 100])
+    ax1.xaxis.set_major_locator(MaxNLocator(11))
+    ax1.xaxis.grid(True, linestyle='--', which='major',
+                   color='grey', alpha=.25)
+
+    # Plot a solid vertical gridline to highlight the median position
+    ax1.axvline(50, color='grey', alpha=0.25)
+
+    # Set the right-hand Y-axis ticks and labels
+    ax2 = ax1.twinx()
+
+    scoreLabels = [format_score(scores[k].score, k) for k in testNames]
+
+    # set the tick locations
+    ax2.set_yticks(pos)
+    # make sure that the limits are set equally on both yaxis so the
+    # ticks line up
+    ax2.set_ylim(ax1.get_ylim())
+
+    # set the tick labels
+    ax2.set_yticklabels(scoreLabels)
+
+    ax2.set_ylabel('Test Scores')
+
+    xlabel = ('Percentile Ranking Across {grade} Grade {gender}s\n'
+              'Cohort Size: {cohort_size}')
+    ax1.set_xlabel(xlabel.format(grade=attach_ordinal(student.grade),
+                                 gender=student.gender.title(),
+                                 cohort_size=cohort_size))
+
+    rect_labels = []
+    # Lastly, write in the ranking inside each bar to aid in interpretation
+    for rect in rects:
+        # Rectangle widths are already integer-valued but are floating
+        # type, so it helps to remove the trailing decimal point and 0 by
+        # converting width to int type
+        width = int(rect.get_width())
+
+        rankStr = attach_ordinal(width)
+        # The bars aren't wide enough to print the ranking inside
+        if width < 40:
+            # Shift the text to the right side of the right edge
+            xloc = 5
+            # Black against white background
+            clr = 'black'
+            align = 'left'
+        else:
+            # Shift the text to the left side of the right edge
+            xloc = -5
+            # White on magenta
+            clr = 'white'
+            align = 'right'
+
+        # Center the text vertically in the bar
+        yloc = rect.get_y() + rect.get_height() / 2
+        label = ax1.annotate(rankStr, xy=(width, yloc), xytext=(xloc, 0),
+                            textcoords="offset points",
+                            ha=align, va='center',
+                            color=clr, weight='bold', clip_on=True)
+        rect_labels.append(label)
+
+    # make the interactive mouse over give the bar title
+    ax2.fmt_ydata = format_ycursor
+    # return all of the artists created
+    return {'fig': fig,
+            'ax': ax1,
+            'ax_right': ax2,
+            'bars': rects,
+            'perc_labels': rect_labels}
+
+
 if __name__ == "__main__":
-    d = get_github_data()
-    timeline(d[0], d[1])
+    np.random.seed(42)
+
+    Student = namedtuple('Student', ['name', 'grade', 'gender'])
+    sale = namedtuple('Sale', ['name', 'team', 'gender'])
+    Score = namedtuple('Score', ['score', 'percentile'])
+
+
+    # GLOBAL CONSTANTS
+    testNames = ['Pacer Test', 'Flexed Arm\n Hang', 'Mile Run', 'Agility',
+                 'Push Ups']
+    product_names = ['肺癌专项版90基因检测',
+                    'HapOnco 肿瘤605基因临床用药守护计划（再次检测）',
+                    'HapOnco 肺癌专项版90基因检测',
+                    'HapOnco 肺癌基础版11基因检测',
+                    '肺癌耐药检测(T790M)',
+                    '结直肠癌临床用药28基因检测',
+                    '肿瘤临床用药451基因检测',
+                    'HapOnco 肿瘤605基因临床用药守护计划（首次检测）',
+                    'WESPlus',
+                    '遗传性肿瘤58基因检测',
+                    'HapOnco 肺癌临床用药11基因检测（赠送PD-L1和MSI）',
+                    '消化道肿瘤51基因检测',
+                    'HapOnco 肿瘤临床用药605基因（单样本单次检测）',
+                    'HapOnco\xa0 消化道肿瘤51基因检测',
+                    '肺癌靶向用药31基因检测',
+                    'HapOnco 肿瘤临床用药605基因（双样本单次检测）',
+                    'HapOnco WESPlus 单次检测',
+                    'HapOnco 肺癌临床用药90基因检测（赠送PD-L1和MSI）',
+                    '肺癌基础版11基因检测',
+                    '实体瘤605基因检测']
+    testMeta = dict(zip(testNames, ['laps', 'sec', 'min:sec', 'sec', '']))
+    productMeta = dict(zip(testNames, ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']))
+
+    student = Student('Johnny Doe', 2, 'boy')
+    scores = dict(zip(testNames,
+                      (Score(v, p) for v, p in
+                       zip(['7', '48', '12:52', '17', '14'], np.round(np.random.uniform(0, 1, len(testNames)) * 100, 0)))))
+    cohort_size = 62  # The number of other 2nd grade boys
+
+    arts = plot_student_results(student, scores, cohort_size)
+
     # fig = plt.figure(figsize=(3, 2.5))
     # fig.subplots_adjust(top=0.8)
     # ax1 = setup_axes(fig, "111")
@@ -418,4 +586,4 @@ if __name__ == "__main__":
     #         my_scatter(ax[j], df.loc[:, x[j]], df.loc[:, y[0]], {})
     # else:
     #     pass
-    # plt.savefig("/home/murphy/django/static/images/stat.png")
+    plt.savefig("/home/murphy/django/static/images/stat.png")
